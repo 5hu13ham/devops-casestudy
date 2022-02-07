64fbfa6dc3e6b9b492fef7b98becca2982def93f
NiharikaPalle <palleniharikajsn@gmail.com>
2022-02-07 15:37:59 +0530

    update

--- devops-casestudy/src/main/java/com/spring/myapp/DevopsCasestudyApplication.java
+++ devops-casestudy/src/main/java/com/spring/myapp/DevopsCasestudyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsCasestudyApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsCasestudyApplication.class, args);
